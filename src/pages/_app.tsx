fba82462fd71a8b436e8004e2c6cb5d8493a8bc5
brunooomelo <bruno94@outlook.com>
2023-07-10 13:24:27 -0300

    chore: add meta tag to google verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,6 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Crie e envie modelo de orçamento para seu cliente de forma rapida e pratica, recebendo pagamento em alguns cliques."
         />
         <meta name="ahrefs-site-verification" content="0ff7ff9de03f88bcd24fe4511f725298dc7d90f3c915636975575d8b463daf90"></meta>
+        <meta name="google-site-verification" content="u5_Q-zBHGVK6bNT5-QHCHY48fxC-IoqAxYo-H2BLhJQ" />
       </Head>
       <main className={`${font.className} container mx-auto px-8 py-20 text-center flex flex-col gap-8`}>
         <Component {...pageProps} />
